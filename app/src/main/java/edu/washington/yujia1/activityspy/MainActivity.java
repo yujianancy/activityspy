2bb1554ff9afbbd2757978b5894919608e6d92c0
Nancy <yujia151992@gmail.com>
2015-04-09 21:26:22 -0700

    Assignment 3

---
+++ app/src/main/java/edu/washington/yujia1/activityspy/MainActivity.java
@@ -0,0 +1,49 @@
+package edu.washington.yujia1.activityspy;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.util.Log;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("Main Activity","onCreate event fired.");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.i("Main Activity","onCreateOptionsMenu event fired.");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        Log.i("Main Activity","onOptionsItemSelected event fired.");
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.e("Main Activity","We’re going down, Captain!");
+    }
+}
